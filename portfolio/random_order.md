c0a28967ae08636450b870227d17f0d02fe6fd80
Christiam Hurtado <crhurtado@users.noreply.github.com>
2017-02-28 12:46:12 -0300

    Update and rename random_order.md to random_order.php

--- portfolio/random_order.md
+++
@@ -1,5 +0,0 @@
-seach 
-/public_html/demos/wp-content/themes/Divi/includes/builder/main-modules.php
-
-add " 'orderby' => 'rand', "
-into $query_args and $default_query_args (before // Get portfolio query)
---
+++ portfolio/random_order.php
@@ -0,0 +1,9 @@
+//search 
+// "/public_html/demos/wp-content/themes/Divi/includes/builder/main-modules.php"
+
+//add 
+" 'orderby' => 'rand', "
+
+//into 
+$query_args and $default_query_args 
+//(before // Get portfolio query)
